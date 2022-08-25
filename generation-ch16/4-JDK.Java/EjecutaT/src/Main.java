27b9183a446f92a4ddd1139904814373ccdedd9a
unknown <alonsojorge111@gmail.com>
2022-08-25 15:18:51 -0500

    PRUEBA NUEVO EQUIPO

---
+++ generation-ch16/4-JDK.Java/EjecutaT/src/Main.java
@@ -0,0 +1,10 @@
+package MetPrincipal;
+
+import MenuP.MenuOp;
+
+public class EjecutaT {
+    public static void main(String[] args) {
+        MenuOp menuOp = new MenuOp();
+        menuOp.menu();
+    }
+}
