3adc00e372955e53852e4570bda12fcd58734242
Jorge Tello <alonsojorge111@gmail.com>
2022-09-12 17:35:41 -0500

    CLASE 12-09-22

---
+++ generation-ch16/7-Spring/EjemploMVC/src/main/java/com/generationmvc/EjemploMvcApplication.java
@@ -0,0 +1,13 @@
+package com.generationmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploMvcApplication.class, args);
+	}
+
+}
---
+++ generation-ch16/7-Spring/Hola/src/main/java/com/generationjta/HolaMundo.java
@@ -0,0 +1,18 @@
+package com.generationjta;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+
+public class HolaMundo {
+	/*@GetMapping("/index")
+	
+	public String hola () {
+		return "index";
+		
+	}*/
+}
